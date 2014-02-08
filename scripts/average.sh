5b8be328246718b2dcd8c5676eda6dcdc85c0e83
Nikhil Sri Ram <nikhilsriram05@gmail.com>
2014-02-08 22:54:48 +0530

    Adjusted average.sh to produce average instead of sums

--- scripts/average.sh
+++ scripts/average.sh
@@ -16,7 +16,7 @@ chmod 666 $output_file
 while (( iter_no <= total_iter ))
 do
 	echo -n $iter_no >> $output_file
-	cat $input_file | sed -nr "s/^$iter_no,/&/p" | awk -F "," '{sum1+=$3;sum2+=$4;sum3+=$5;sum4+=$6;sum5+=$7;}END{printf " "sum1" "sum2" "sum3" "sum4" "sum5"\n"}' >> $output_file
+	cat $input_file | sed -nr "s/^$iter_no,/&/p" | awk -vrer=$no_reruns -F "," '{sum1+=$3;sum2+=$4;sum3+=$5;sum4+=$6;sum5+=$7;}END{printf " "sum1/rer" "sum2/rer" "sum3/rer" "sum4/rer" "sum5/rer"\n"}' >> $output_file
 
 	(( iter_no += 1 ))
 
